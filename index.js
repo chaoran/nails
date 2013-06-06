442d56272c6cb83a4ca61d01421e2444a1b3cf02
Chaoran Yang <chaorany@me.com>
2013-06-06 16:05:21 -0500

    code cleanup

--- index.js
+++ index.js
@@ -1,57 +1,8 @@
-var path = require('path')
-  , fs = require('fs')
+var currentApp;
 
-var cached;
-var neutron = function(root) {
-  if (cached) return cached;
-  if (!root) root = process.cwd();
-
-  var pkg;
-  do {
-    var pkgPath = path.join(root, 'package.json');
-    if (fs.existsSync(pkgPath)) {
-      pkg = require(pkgPath);
-      if (pkg.dependencies.neutron) break;
-    }
-  } while ((root = path.dirname(root)) != '/');
-
-  if (root === '/') 
-    require('./lib/say').fatal('current direcotry is not a neutron app');
-  else 
-    root = path.relative(process.cwd(), root);
-
-  var app = {
-    root: root,
-    name: pkg.name,
-    version: pkg.version,
-    env: process.env.NODE_ENV || 'development'
-  };
-
-  Object.defineProperty(app, 'config', {
-    get: function() {
-      delete this.config;
-      return this.config = require('./lib/config')(app);
-    },
-    configurable: true
-  });
-
-  Object.defineProperty(app, 'adapter', {
-    get: function() {
-      delete this.adapter;
-      return this.adapter = require('./lib/adapter')(app.config.database);
-    },
-    configurable: true
-  });
-
-  Object.defineProperty(app, 'migrator', {
-    get: function() {
-      delete this.migrator;
-      return this.migrator = require('./lib/migrator')(app);
-    },
-    configurable: true
-  });
-
-  return cached = app;
+var neutron = module.exports = function() {
+  if (!currentApp) currentApp = locateApp();
+  return currentApp;
 };
 
 Object.defineProperty(neutron, 'Jakefile', {
@@ -62,4 +13,22 @@ Object.defineProperty(neutron, 'Jakefile', {
   configurable: true
 });
 
-module.exports = neutron;
+function locateApp() {
+  var path = require('path')
+    , fs = require('fs')
+    , root = process.cwd()
+    , package;
+
+  do {
+    var packagePath = path.join(root, 'package.json');
+    if (fs.existsSync(packagePath )) {
+      package = require(packagePath);
+      if (package.dependencies.neutron) break;
+    }
+  } while ((root = path.dirname(root)) != '/');
+
+  if (root === '/') throw new Error('cannot find app root directory');
+  else root = path.relative(process.cwd(), root);
+
+  return new (require('./lib/app'))(root, package);
+}
