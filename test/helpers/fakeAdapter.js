bdd27bea84c8c7ec0589688e15d72fecd6647012
Chaoran Yang <chaorany@me.com>
2013-06-21 02:12:22 -0500

    fix transactions. bump version.

--- test/helpers/fakeAdapter.js
+++ test/helpers/fakeAdapter.js
@@ -1,5 +1,7 @@
 var Adapter = require('../../lib/adapter');
 var assert = require('assert');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
 var FakeAdapter = module.exports = Adapter.extend(function(config) {
   this.Parser = function() {
@@ -19,30 +21,29 @@ FakeConnection.prototype.query = function(q, callback) {
   this.logs.push(q);
 
   if (typeof q !== 'string') {
-    if (callback) return callback(null);
-    else return;
-  }
-
-  if (q.indexOf('SELECT v FROM schema_migrations') == 0) {
-    if (this.schemas.length === 0) 
-      return callback(new Error('does not exists'));
+    if (callback) callback(null);
+  } else if (q.indexOf('SELECT v FROM schema_migrations') == 0) {
+    if (this.schemas.length === 0) callback(new Error('does not exists'));
     else {
       var v = this.schemas[this.schemas.length - 1];
-      return callback(null, [{ v: v }]);
+      callback(null, [{ v: v }]);
     }
-  }
-
-  if (q.indexOf('INSERT INTO schema_migrations') == 0) {
+    callback = null;
+  } else if (q.indexOf('INSERT INTO schema_migrations') == 0) {
     this.schemas.push(q.match(/[0-9]{14}/)[0]);
-  }
-
-  if (q.indexOf('DELETE FROM schema_migrations') == 0) {
+  } else if (q.indexOf('DELETE FROM schema_migrations') == 0) {
     var v = q.match(/[0-9]{14}/)[0];
     var index = this.schemas.indexOf(v);
     this.schemas.splice(index, 1);
   }
 
-  if (callback) callback(null);
+  var e = new EventEmitter();
+  process.nextTick(function() {
+    e.emit('end');
+    if (callback) callback(null);
+  });
+
+  return e;
 };
 
 FakeAdapter.prototype.connect = function(callback) {
